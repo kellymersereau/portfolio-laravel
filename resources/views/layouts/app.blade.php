27eb1de997b69fa4217f0882a5bbdb093c0c2e64
Kelly M <kmersereau@besentient.com>
2017-09-19 23:01:05 -0400

    setting up laravel

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>@yield('title')</title>
+		<link rel="stylesheet" type="text/css" href="/css/app.css">
+	</head>
+	<body>
+		<div class="container">
+			@yield('content')
+		</div>
+	
+		<script type="text/javascript" src="/js/app.js"></script>
+	</body>
+</html>
